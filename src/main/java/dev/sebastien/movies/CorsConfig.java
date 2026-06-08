8792b563c29c3612940b62745ce80386adf37294
@sebastien <sebastienrandriamanana3@gmail.com>
2026-06-08 01:34:24 +0100

    update: CorsConfig

--- src/main/java/dev/sebastien/movies/CorsConfig.java
+++ src/main/java/dev/sebastien/movies/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://reelifymovies.vercel.app/"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
 
